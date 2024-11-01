170cc50c55150c593091fa7fbb89c025a8fdca27
nicollyADS <adm.nicolly@gmail.com>
2024-10-31 22:49:37 -0300

    iniciando projeto

---
+++ src/main/java/br/com/mapped/caremi/CaremiApplication.java
@@ -0,0 +1,13 @@
+package br.com.mapped.caremi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaremiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaremiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mapped/caremi/CaremiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mapped.caremi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaremiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
